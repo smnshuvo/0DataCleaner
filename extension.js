edd123155b21671b6d0cf42583996e70a430e4b0
Shuvo <smnshuvo@gmail.com>
2021-12-06 23:38:42 +0600

    Created a few methods for context menu

--- extension.js
+++ extension.js
@@ -12,15 +12,18 @@ function activate(context) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "javascript" is now active!');
+	console.log('0DateCleaner is now running!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('0data.helloWorld', showWelcomeMessage);
 	let loadExcel = vscode.commands.registerCommand('0data.loadExcel', loadFile);
+		let handleMissingValues = vscode.commands.registerCommand('0data.handleMissing',(uri) => {
+			HandleMissingValues(uri.fsPath);
+		});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(handleMissingValues);
 	context.subscriptions.push(loadExcel);
 }
 
@@ -30,8 +33,21 @@ function showWelcomeMessage(){
 
 }
 
+function HandleMissingValues(filePath){
+	console.log(filePath);
+}
+
 function loadFile(){
+	var xlsx = require('node-xlsx');
+	xlsx.parse('C:/Users/Shuvo/Desktop/New folder/test.xlsx');
 	vscode.window.showInformationMessage('Excel file is being loaded!');
+	getFilePath();
+}
+
+
+async function getFilePath(){	
+		let selectedFile = await vscode.window.showOpenDialog();
+		return selectedFile;
 }
 
 // this method is called when your extension is deactivated
